9c0bdc81dcaafcf14662b13bd1d9eed3b35be666
Luis Correia <lg.correia@campus.fct.unl.pt>
2020-09-09 02:25:55 +0100

    Continuing dev of dashboard, navigation footer

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import { AppLoading } from "expo";
-import * as Font from "expo-font"
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font"
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, } from '@react-navigation/stack';
 import {
@@ -10,40 +10,44 @@ import {
 import SettingsView from './components/Exercise/SettingsView';
 import SensorVis from './components/Exercise/SensorVis';
 import Dashboard from './components/Dashboard/Dashboard';
+import { FontDisplay } from 'expo-font';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 //import SensorDataView from './components/SensorDataView';
 
-
-const getFonts = () => Font.loadAsync({
-  'rawline-regular': require("./assets/fonts/rawline-300.ttf")
-})
 
 
 const App = () => {
 
   const { Navigator, Screen } = createStackNavigator();
-  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  if (!fontsLoaded) {
-    return (
-      <AppLoading
-        startAsync={getFonts}
-        onFinish={() => setFontsLoaded(true)}
-      />
-    )
-  } else {
+  const Tab = createMaterialBottomTabNavigator();
+
+  const [loaded] = useFonts({
+    "Rawline-Light": require("./assets/fonts/rawline-300.ttf"),
+    "Rawline": require("./assets/fonts/rawline-400.ttf"),
+    "Rawline-Bold": require("./assets/fonts/rawline-600.ttf"),
+    "Rawline-Black": require("./assets/fonts/rawline-900.ttf")
+  });
+
+  // if (!loaded) {
+  //   return <View>
+  //     <Text>Loading ...</Text>
+  //   </View>;
+  // } else {
+
     return (
       <>
         <NavigationContainer>
-          <Navigator headerMode="none" initialRouteName="Dashboard">
-            <Screen name="Dashboard" component={Dashboard} />
-            <Screen name="SensorVis" component={SensorVis} />
-            <Screen name="SettingsView" component={SettingsView} />
-          </Navigator>
+          <Tab.Navigator initialRouteName="Início" activeColor="#736DFD" inactiveColor="#D7D6FF" labeled shifting={false} barStyle={{ backgroundColor: '#FFF', borderTopColor: "#DDD", borderStyle: "solid", borderTopWidth: 1 }}>
+            <Tab.Screen name="Médicos" component={SensorVis}/>
+            <Tab.Screen name="Consultas" component={SensorVis} />
+            <Tab.Screen name="SensorVis" component={SensorVis} />
+            <Tab.Screen name="Início" component={Dashboard} />
+            <Tab.Screen name="Definições" component={SettingsView} />
+          </Tab.Navigator>
         </NavigationContainer>
       </>
     );
-  }
-
 }
 
 export default App;
--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import {
     Button
 } from 'react-native-elements';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import SettingsView from '../Exercise/SettingsView';
 import SensorVis from '../Exercise/SensorVis';
 //import SensorDataView from './components/SensorDataView';
@@ -15,8 +17,11 @@ export const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        fontFamily: 'Rawline'
     },
 });
+
+
 
 
 export default function Dashboard({ navigation }) {
@@ -29,31 +34,140 @@ export default function Dashboard({ navigation }) {
         navigation.navigate("SensorVis", { styles })
     }
 
+
     return (
         <>
             <View style={{
+                flex: 1,
+                flexWrap: 'wrap',
                 flexDirection: "row",
-                backgroundColor: "#5751DB",
-                height: "25%",
-                width: "100%",
-                padding: 25,
+                maxHeight: "30%"
             }}>
-                <View style={{ }}>
-                    <Text style={{ color: "white", fontSize: 22 }}>Olá</Text>
-                    <Text style={{ color: "white", fontSize: 33, fontWeight: "bold" }}>Joana!</Text>
-
+                <LinearGradient
+                    // Background Linear Gradient
+                    colors={['rgba(87,81,219,1.0)', 'rgba(155,152,233,1.0)']}
+                    start={[0, 1]}
+                    end={[0.8, 0]}
+                    style={{
+                        position: 'absolute',
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        height: "100%"
+                    }}
+                />
+                <View style={{
+                    flexDirection: "row",
+                    padding: 15,
+                    alignContent: "center",
+                }}>
+                    <View style={{ alignSelf: "flex-start", flexBasis: "70%" }}>
+                        <Text style={{ fontFamily: "Rawline", color: "white", fontSize: 22, }}>Olá</Text>
+                        <Text style={{ fontFamily: "Rawline-Bold", color: "white", fontSize: 25, lineHeight: 27 }}>Joana!</Text>
+                    </View>
+                    <View style={{ alignSelf: "flex-end", flexBasis: "30%" }}>
+                        <Image source={require("../../assets/images/user.png")} style={{ height: 70, width: 70, borderRadius: 70 / 2, alignSelf: "center" }} />
+                    </View>
                 </View>
-                <View style={{ alignSelf: "center", width: "50%" }}></View>
-                <View style={{ alignSelf: "stretch" }}>
-                    <Image source={require("../../assets/images/user.png")} style={{ height: 70, width: 70, borderRadius: 70 / 2 }} />
+                <View style={{
+                    flexDirection: "row",
+                    paddingLeft: 15,
+                    paddingRight: 15,
+                    alignContent: "center",
+                }}>
+                    <View style={{ alignSelf: "flex-start", flexDirection: "row", flexBasis: "100%" }}>
+                        <Text style={{ fontFamily: "Rawline", color: "white", fontSize: 15 }}>Progresso da semana</Text>
+                    </View>
                 </View>
+                <View style={{
+                    flexDirection: "row",
+                    marginTop: 5,
+                    height: 50,
+                    marginLeft: 15,
+                    marginRight: 15,
+                    alignContent: "center",
+                }}>
+                    <View style={{
+                        alignSelf: "flex-start", flexDirection: "row", height: 50,
+                        flexBasis: "100%", backgroundColor: "rgba(165,162,240,0.78)",
+                        borderRadius: 10,
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 5,
+                        },
+                        shadowOpacity: 0.36,
+                        shadowRadius: 6.68,
+                        elevation: 11
+                    }}>
+                        <View style={{
+                            alignSelf: "flex-start", width: "8%", height: 50,
+                            backgroundColor: "rgba(120,75,250,1.0)",
+                            borderRadius: 10,
+                            shadowColor: "#FFFFFF",
+                            shadowOffset: {
+                                width: 5,
+                                height: 5,
+                            },
+                            shadowOpacity: 1.0,
+                            shadowRadius: 6.68,
+                            elevation: 3
+                        }}>
+                            <Text style={{ fontFamily: "Rawline-Bold", color: "white", fontSize: 15, textAlignVertical: "center", alignSelf: "center" }}>0%</Text>
+                        </View>
+                    </View>
+                </View >
             </View >
-            <View style={styles.container}>
+
+            <View style={{
+                flex: 1,
+                flexWrap: 'wrap',
+                flexDirection: "row",
+                maxHeight: "11%",
+                padding: 10,
+                backgroundColor: "white"
+            }}>
+                <View style={{
+                    alignSelf: "flex-start", flexDirection: "row", height: 50,
+                    flexBasis: "100%", backgroundColor: "rgba(215,214,255,1.0)",
+                    borderRadius: 10, paddingLeft: 10
+                }}>
+                    <Text style={{ alignSelf: "center", color: "#6F6CB0" }}>Última notificação</Text>
+                </View>
+            </View>
+
+            <View style={{
+                flex: 1,
+                flexWrap: 'wrap',
+                flexDirection: "row",
+                maxHeight: "18%",
+                paddingLeft: 10,
+                backgroundColor: "white"
+            }}>
+                <View style={{
+                    alignSelf: "flex-start", flexDirection: "row", height: "100%",
+                    flexBasis: "49%", backgroundColor: "#8582E4",
+                    borderRadius: 10, paddingLeft: 10, paddingRight: 5
+                }}>
+                    <Text style={{ alignSelf: "center", color: "#FFF" }}>Próxima Consulta</Text>
+                </View>
+                <View style={{
+                    alignSelf: "flex-start", flexDirection: "row", height: "100%",
+                    flexBasis: "47%", backgroundColor: "rgba(215,214,255,1.0)",
+                    borderRadius: 10, paddingLeft: 5, paddingRight: 5, marginLeft: 5
+                }}>
+                    <Text style={{ alignSelf: "center", color: "#6F6CB0" }}>Próxima Actividade</Text>
+                </View>
+            </View>
+
+
+
+            {/* <View style={styles.container}>
                 <Text>This app test wifi network switching.</Text>
                 <Text>Go to settings</Text>
                 <Button title="Settings" onPress={changeToSettings} />
                 <Button title="3D Vis" onPress={changeToVis} />
-            </View>
+            </View> */}
         </>
     );
 }
